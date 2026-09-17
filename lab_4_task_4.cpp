82189334e14c5d9c5bdd7f03252b5ee446807f99
Сергей Якимович <serginur97@gmail.com>
2026-09-17 13:55:45 +0300

    Исправление алгоритма макаронины и изменение вывода answer()

--- lab_4_task_4.cpp
+++ lab_4_task_4.cpp
@@ -3,26 +3,27 @@
 #include <vector>
 
 void answer(const uint8_t& is_it) {
-    std::string no = (is_it == 0) ? " не " : "  ";
+    std::string no = (is_it == 0) ? " не " : " ";
     std::cout << "Данная точка" << no << "входит в заштрихованную область.\n";
 }
 
 int main() {
-    std::vector<std::string> tokens = input_coordinates("Введите координаты точки x и y:\n", 2);
+    std::vector<std::string> tokens = input_coordinates("Введите координаты точки x и y: ", 2);
     double x = std::stod(tokens.at(0));
     double y = std::stod(tokens.at(1));
 
-    if (pow(y, 2.0) + pow(y, 2.0) <= 4) {
+    if (pow(x, 2.0) + pow(y, 2.0) < 4) {
         answer(0);
-    } else if ((x > 0 && y > 0) && (x <= 4 && y <= 4)) {
+    } else if (pow(x, 2.0) + pow(y, 2.0) == 4) {
         answer(1);
-    } else if ((x < 0 && y < 0) && (-x - y <= 4)) {
+    } else if ((x >= 0 && y >= 0) && (x <= 4 && y <= 4)) {
+        answer(1);
+    } else if ((x <= 0 && y <= 0) && (-x - y <= 16)) {
         answer(1);
     } else if (((x < 0 && y > 0) || (x > 0 && y < 0)) && (pow(x, 2.0) + pow(y, 2.0) <= 16)) {
         answer(1);
     } else {
         answer(0);
     }
-
     return 0;
 }
