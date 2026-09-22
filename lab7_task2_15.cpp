9822c0f70f162e84ad2930fcb2469ec69478900d
Сергей Якимович <serginur97@gmail.com>
2026-09-22 07:56:52 +0300

    Предварительный вариант решения

--- lab7_task2_15.cpp
+++ lab7_task2_15.cpp
@@ -8,3 +8,18 @@
    |0|0|0| |
    |4|4|4|4|
    |0|0|0|0|*/
+
+#include <iostream>
+
+int main() {
+   std::cout << "_____" << std::endl;
+   for (int i = 1; i <= 4; ++i) {
+      for (int j = 0; j < 1; ++j) {
+         std::cout << "|";
+         for (int k = 0; k < 4; ++k) {
+
+         }
+      }
+   }
+   std::cout << "–––––" << std::endl;
+}
